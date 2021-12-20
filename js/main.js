6487a1a45ba439c95128c425d47b4347ba44838e
four yellow <55367250+four-yellow@users.noreply.github.com>
2021-12-20 17:45:24 -0500

    no transition on page load

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@
 function goTo(page){
     document.body.className = page;
 }
-
-goTo(window.location.hash.slice(1));
+document.body.className = window.location.hash.slice(1) + " initial";
+setTimeout(()=> {
+    goTo(window.location.hash.slice(1));
+}, 200);
